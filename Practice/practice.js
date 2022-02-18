fb625242c9b740b4c2035d3b4e34a810565f3613
Jihyo Kang <luxuryjihyo@naver.com>
2022-02-18 10:56:50 +0900

    18 Feb 2022

--- Practice/practice.js
+++ Practice/practice.js
@@ -22,27 +22,45 @@ let taskList = []
 addButton.addEventListener("click",addTask)
 
 function addTask(){
-    let taskContents = taskInput.value;
-    taskList.push(taskContents)
-    console.log(taskList)
+    let task = {
+        id : ranId(),
+        taskContents : taskInput.value,
+        isComplete : false
+    }
+    taskList.push(task)
+    console.log(task)
     render() 
 }
 
 function render(){
     let resultHTML = "";
     for(let i =0 ; i < taskList.length ; i++){
-        resultHTML +=`<section class="task-bar">
-        <div>${taskList[i]}</div>
-        <div>
-            <button>Check</button>
-            <button>Delete</button>
-        </div>
-    </section>
-    <div class="task-underline"></div>`
+        if(taskList[i].isComplete == true){
+            resultHTML +=
+            `<section class="task-bar">
+                <div class="doneCheck">${taskList[i].taskContents}</div>
+                <div>
+                <button onclick="toggleButton('${taskList[i].id}')">Check</button>
+                <button onclick="deleteButton('${taskList[i].id}')">Delete</button>
+                </div>
+            </section>
+            <div class="task-underline"></div>`
+        }else{
+            resultHTML +=
+            `<section class="task-bar">
+                <div>${taskList[i].taskContents}</div>
+                <div>
+                    <button onclick="toggleButton('${taskList[i].id}')">Check</button> 
+                    <button onclick="deleteButton('${taskList[i].id}')">Delete</button>
+                </div>
+            </section>
+            <div class="task-underline"></div>`
+        }
     }
 
     document.getElementById("task-board").innerHTML = resultHTML
 }
+// onclick="함수명()" 은 html에서 바로 함수를 불러올수 있는 클릭이벤트
 
 // 입력창 선택시 값 삭제
 
@@ -54,4 +72,30 @@ function enterKey(){
     if (window.event.keyCode == 13){
         addTask()
     }
+}
+
+//랜덤 아이디 생성
+function ranId(){
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+//체크버튼
+function toggleButton(id){
+    for(let i = 0 ; i < taskList.length ; i++){
+        if(taskList[i].id == id){
+            taskList[i].isComplete = !taskList[i].isComplete
+        }
+    }
+    render()
+}
+
+
+//삭제버튼
+function deleteButton(id){
+    for(let i = 0 ; i < taskList.length ; i++){
+        if(taskList[i].id==id){
+            taskList.splice(i,1)
+        } 
+    }
+    render()
 }
--- To do list (Object)/main.js
+++ To do list (Object)/main.js
@@ -26,27 +26,60 @@ function addTask(){
 function render(){
     let resultHTML = " "
     for(let i=0; i<taskList.length ; i++){
-        resultHTML += `
+        if(taskList[i].isComplete == true){
+            resultHTML += `
+        <div id = "task-board">
+            <div class = "task-done">
+                ${taskList[i].taskContent}
+            </div>
+            <div>
+                <button onclick="toggleButton('${taskList[i].id}')">Check</button>
+                <button id="delete-button">Delete</button>
+            </div>
+        </div>`
+        } else{
+            resultHTML += `
         <div id = "task-board">
             <div>
                 ${taskList[i].taskContent}
             </div>
             <div>
-                <button onclick="checkThrough('${taskList[i].id}')">Check</button>
-                <button id="delete-button">Delete</button>
+                <button onclick="toggleButton('${taskList[i].id}')">Check</button>
+                <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
             </div>
         </div>`
+        }
+        
     }
 
     document.getElementById("task-area").innerHTML = resultHTML
 }
 
-function checkThrough(id){
-    console.log(id)
+
+//객체 색인 및 설정변경
+function toggleButton(id){
+    for(let i = 0 ; i < taskList.length ; i++){
+        if(taskList[i].id == id ){
+            taskList[i].isComplete = !taskList[i].isComplete //!뭐시기 는 반대값을 나타냄 (false -> true, true -> false)
+            break
+        }
+    }
+    render()
 }
 
+
+function deleteTask(id){
+    for(let i = 0 ; i < taskList.length ; i++){
+        if(taskList[i].id==id){
+            taskList.splice(i,1)
+            break
+        }
+    }
+    render()
+}
 
 // id 생성함수
 function randomId(){
     return '_' + Math.random().toString(36).substring(2, 9);
 }
+
